8ae2dea20b821851bbb9af727a89768643552ca4
beamelement <827907398@qq.com>
2021-02-06 16:24:54 +0800

    功能完成

--- hangerRod/Command.cs
+++ hangerRod/Command.cs
@@ -37,7 +37,7 @@ namespace hangerRod
             while (!window1.Done)
             {
 
-                //选择一个腹杆
+                //选择横梁
                 if (window1.Selected)
                 {
                     Selection sel = uiDoc.Selection;
@@ -47,51 +47,42 @@ namespace hangerRod
 
                     using (Transaction tran = new Transaction(uiDoc.Document))
                     {
-                        tran.Start("选中横梁");
+                        tran.Start("创建吊杆模型线");
 
-                        //初试颜色
-                        Material materialOrigin = uiDoc.Document.GetElement(familyInstance.GetMaterialIds(false).First()) as Material;
-                        colorOrigin = materialOrigin.Color;
+                        //改变选中实例轮廓颜色
+                        colorOrigin = changeProfileColor(familyInstance,commandData);
 
-                        //改变选中实例的线的颜色
-                        OverrideGraphicSettings overrideGraphicSettings = new OverrideGraphicSettings();
-                        overrideGraphicSettings = uiDoc.Document.ActiveView.GetElementOverrides(familyInstance.Id);
+                        //创建吊杆所要用到的模型线
+                        List<ModelLine>modelLines = createHangerROdModelLines(familyInstance,commandData);
+                        //注意此处模型线底部高度是随意设置的，到时候要进行修改
 
-                        Color color = new Color(255, 0, 0);
-                        overrideGraphicSettings.SetProjectionLineColor(color);
-                        //在当前视图下设置，其它视图保持原来的
-                        uiDoc.Document.ActiveView.SetElementOverrides(familyInstance.Id, overrideGraphicSettings);
-                        uiDoc.Document.Regenerate();
+                        FamilySymbol familySymbol;
+                        //载入吊杆族
+                        string file = @"C:\Users\zyx\Desktop\2RevitArcBridge\hangerRod\hangerRod\source\hangerRod.rfa";
+                        familySymbol = loadFaimly(file, commandData);
+                        familySymbol.Activate();
 
-
-
-                        //获取吊杆底部坐标
-                        XYZ instancePoint = (familyInstance.Location as LocationPoint).Point;//实例位置
-                        XYZ instanceNormalVec = familyInstance.FacingOrientation; //实例的法向量
-                        XYZ instanceTangentVec = instanceNormalVec.CrossProduct(XYZ.BasisZ);//实例切向量
-                        double tangentDistance = familyInstance.LookupParameter("l1/2").AsDouble() + familyInstance.LookupParameter("l2").AsDouble() + familyInstance.LookupParameter("l3").AsDouble() / 2; //获取吊杆圆心坐标
-                        double ZDistance = 10;//竖向的偏移距离，之后有时间可以考虑把他设坐参数
-                        XYZ ptemp = (instancePoint + instanceTangentVec * tangentDistance);
-                        XYZ hangerRodDownPoint = new XYZ(ptemp.X, ptemp.Y, ptemp.Z - ZDistance);//横梁底部坐标
+                        ////创建族实例
+                        //createHangerRod(modelLine, familySymbol, commandData); 创建基于线的族实例失效，但是已经定义好工作平面，此步可以通过手动实现
 
 
+                        //创建并激活形成一个工作平面
+                        XYZ p1 = modelLines[0].GeometryCurve.Evaluate(0, true);
+                        XYZ p2 = modelLines[0].GeometryCurve.Evaluate(1, true);
+                        XYZ p3 = modelLines[1].GeometryCurve.Evaluate(0, true);
+                        XYZ p4 = modelLines[1].GeometryCurve.Evaluate(1, true);
 
-                        //获取吊杆顶部坐标
-                        View3D view3d = uiDoc.Document.ActiveView as View3D;
-                        List<FamilyInstance> instances = ReferenceIntersectElement(uiDoc.Document, view3d, hangerRodDownPoint, XYZ.BasisZ);//射线法，找元素
-                        FamilyInstance chordInstance = instances.Last();
-                        XYZ hangerRodUPPoint = Interpolation(chordInstance, hangerRodDownPoint, commandData);//内插法求得插入的点
+                        XYZ vec1 = p1.Subtract(p4);
+                        XYZ vec2 = p2.Subtract(p3);
+                        XYZ normal = vec1.CrossProduct(vec2);
 
-                        //创建模型线
-                        Line hangerRodLine = Line.CreateBound(hangerRodUPPoint, hangerRodDownPoint);
-                        XYZ hangerRodVec = new XYZ(hangerRodUPPoint.X - hangerRodDownPoint.X, hangerRodUPPoint.Y - hangerRodDownPoint.Y, hangerRodUPPoint.Z - hangerRodDownPoint.Z);
-                        XYZ vec = new XYZ(1, 1, 1);
-                        XYZ normal = hangerRodVec.CrossProduct(vec);
-                        Plane plane = Plane.CreateByNormalAndOrigin(normal, hangerRodUPPoint);
+                        Plane plane = Plane.CreateByNormalAndOrigin(normal, p1);
                         SketchPlane sketchPlane = SketchPlane.Create(uiDoc.Document, plane);
-                        ModelLine modelLine = uiDoc.Document.Create.NewModelCurve(hangerRodLine, sketchPlane) as ModelLine;
 
 
+                        uiDoc.Document.ActiveView.SketchPlane = sketchPlane;
+                        uiDoc.Document.ActiveView.ShowActiveWorkPlane();
+                        Reference reference = sketchPlane.GetPlaneReference();
 
                         tran.Commit();
                     }
@@ -111,46 +102,12 @@ namespace hangerRod
             using (Transaction tran = new Transaction(uiDoc.Document))
             {
                 tran.Start("恢复轮廓线的颜色");
-
-                //恢复标注前的颜色
-
-                OverrideGraphicSettings overrideGraphicSettings = new OverrideGraphicSettings();
-                overrideGraphicSettings = uiDoc.Document.ActiveView.GetElementOverrides(familyInstance.Id);
-                overrideGraphicSettings.SetProjectionLineColor(colorOrigin);
-                //在当前视图下设置，其它视图保持原来的
-                uiDoc.Document.ActiveView.SetElementOverrides(familyInstance.Id, overrideGraphicSettings);
-                uiDoc.Document.Regenerate();
+                
+                //恢复实例轮廓颜色
+                recoverProfileColor(familyInstance, colorOrigin, commandData);
 
                 tran.Commit();
             }
-
-
-
-            //载入族
-            FamilySymbol familySymbol;
-            using (Transaction tran = new Transaction(uiDoc.Document))
-            {
-                tran.Start("载入族");
-
-                //载入弦杆族
-                string file = @"C:\Users\zyx\Desktop\2RevitArcBridge\RevitWindBrace\RevitWindBrace\Source\WindBrace.rfa";
-                familySymbol = loadFaimly(file, commandData);
-                familySymbol.Activate();
-
-                tran.Commit();
-            }
-
-            ////风撑族实例化
-            //using (Transaction tran = new Transaction(uiDoc.Document))
-            //{
-            //    tran.Start("创建风撑");
-
-            //    createWindBrace(PPosition, familySymbol, commandData);
-
-            //    tran.Commit();
-            //}
-
-
 
 
             return Result.Succeeded;
@@ -162,6 +119,95 @@ namespace hangerRod
 
 
 
+
+        private List<ModelLine> createHangerROdModelLines(FamilyInstance familyInstance,ExternalCommandData commandData)
+        {
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;           //取得当前活动文档  
+
+            List<ModelLine> modelLines = new List<ModelLine>();
+
+            for(int i = 0; i < 3; i += 2)
+            {
+                //获取吊杆底部坐标
+                XYZ instancePoint = (familyInstance.Location as LocationPoint).Point;//实例位置
+                XYZ instanceNormalVec = familyInstance.FacingOrientation; //实例的法向量
+                XYZ instanceTangentVec = instanceNormalVec.CrossProduct(XYZ.BasisZ);//实例切向量
+                double tempDistance = familyInstance.LookupParameter("l1/2").AsDouble() + familyInstance.LookupParameter("l2").AsDouble() + familyInstance.LookupParameter("l3").AsDouble() / 2; //获取吊杆圆心坐标
+                double distance = (i - 1) * tempDistance;
+                double ZDistance = 10;//竖向的偏移距离，之后有时间可以考虑把他设坐参数
+                XYZ ptemp = (instancePoint + instanceTangentVec * distance);
+                XYZ hangerRodDownPoint = new XYZ(ptemp.X, ptemp.Y, ptemp.Z - ZDistance);//横梁底部坐标
+
+
+                //获取吊杆顶部坐标
+                View3D view3d = uiDoc.Document.ActiveView as View3D;
+                List<FamilyInstance> instances = ReferenceIntersectElement(uiDoc.Document, view3d, hangerRodDownPoint, XYZ.BasisZ);//射线法，找元素
+                FamilyInstance chordInstance = instances.Last();
+                XYZ hangerRodUPPoint = Interpolation(chordInstance, hangerRodDownPoint, commandData);//内插法求得插入的点
+
+                //创建模型线
+                Line hangerRodLine = Line.CreateBound(hangerRodUPPoint, hangerRodDownPoint);
+                XYZ hangerRodVec = new XYZ(hangerRodUPPoint.X - hangerRodDownPoint.X, hangerRodUPPoint.Y - hangerRodDownPoint.Y, hangerRodUPPoint.Z - hangerRodDownPoint.Z);
+                XYZ vec = new XYZ(1, 1, 1);
+                XYZ normal = hangerRodVec.CrossProduct(vec);
+                Plane plane = Plane.CreateByNormalAndOrigin(normal, hangerRodUPPoint);
+                SketchPlane sketchPlane = SketchPlane.Create(uiDoc.Document, plane);
+                ModelLine modelLine = uiDoc.Document.Create.NewModelCurve(hangerRodLine, sketchPlane) as ModelLine;
+                
+                modelLines.Add(modelLine);
+            }
+
+
+            return modelLines;
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        //恢复选中实例的颜色
+        private void recoverProfileColor(FamilyInstance familyInstance, Color colorOrigin, ExternalCommandData commandData)
+        {
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;           //取得当前活动文档  
+            //恢复标注前的颜色
+            OverrideGraphicSettings overrideGraphicSettings = new OverrideGraphicSettings();
+            overrideGraphicSettings = uiDoc.Document.ActiveView.GetElementOverrides(familyInstance.Id);
+            overrideGraphicSettings.SetProjectionLineColor(colorOrigin);
+            //在当前视图下设置，其它视图保持原来的
+            uiDoc.Document.ActiveView.SetElementOverrides(familyInstance.Id, overrideGraphicSettings);
+            uiDoc.Document.Regenerate();
+        }
+
+        //改变选中实例的颜色(被选中的族实例要已经赋予了材质，这个方法才起作用)
+        private Color changeProfileColor(FamilyInstance familyInstance,ExternalCommandData commandData)
+        {
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;           //取得当前活动文档  
+
+            //初试颜色
+            Material materialOrigin = uiDoc.Document.GetElement(familyInstance.GetMaterialIds(false).First()) as Material;
+            Color colorOrigin = materialOrigin.Color;
+
+            //改变选中实例的线的颜色
+            OverrideGraphicSettings overrideGraphicSettings = new OverrideGraphicSettings();
+            overrideGraphicSettings = uiDoc.Document.ActiveView.GetElementOverrides(familyInstance.Id);
+
+            Color color = new Color(255, 0, 0);
+            overrideGraphicSettings.SetProjectionLineColor(color);
+            //在当前视图下设置，其它视图保持原来的
+            uiDoc.Document.ActiveView.SetElementOverrides(familyInstance.Id, overrideGraphicSettings);
+            uiDoc.Document.Regenerate();
+
+            return colorOrigin;
+
+        }
 
 
         //点的内插
@@ -223,7 +269,7 @@ namespace hangerRod
 
                     Family f = e as Family;
                     //通过名字进行筛选
-                    if (f.Name == "WindBrace")
+                    if (f.Name == "hangerRod")
                     {
                         adaptiveFamilySymbol = uiDoc.Document.GetElement(f.GetFamilySymbolIds().First()) as FamilySymbol;
                     }
@@ -234,23 +280,37 @@ namespace hangerRod
         }
 
 
-        //风箱族实例化
-        private void createWindBrace(List<XYZ> points, FamilySymbol FamilySymbol, ExternalCommandData commandData)
+
+        //吊杆族实例化
+        private void createHangerRod(ModelLine modelLine, FamilySymbol familySymbol, ExternalCommandData commandData)
         {
-            UIDocument uiDoc = commandData.Application.ActiveUIDocument;           //取得当前活动文档    
+            Document revitDoc = commandData.Application.ActiveUIDocument.Document;  //取得文档           
+            Application revitApp = commandData.Application.Application;             //取得应用程序            
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;           //取得当前活动文档      
+            //依据线创建一个包含这条线的面
+            XYZ p1 = modelLine.GeometryCurve.GetEndPoint(0);
+            XYZ p2 = modelLine.GeometryCurve.GetEndPoint(1);
+            XYZ LineVec = new XYZ(p1.X - p2.X, p1.Y - p2.Y, p1.Z - p2.Z);
+            XYZ vectemp = new XYZ(1, 1, 1);
+            XYZ normal = LineVec.CrossProduct(vectemp);
+            Plane plane = Plane.CreateByNormalAndOrigin(normal, p1);
+            SketchPlane sketchPlane = SketchPlane.Create(uiDoc.Document, plane);
+            Reference reference = sketchPlane.GetPlaneReference();
 
-            //创建实例，并获取其自适应点列表
-            FamilyInstance familyInstance = AdaptiveComponentInstanceUtils.CreateAdaptiveComponentInstance(uiDoc.Document, FamilySymbol);
-            IList<ElementId> adaptivePoints = AdaptiveComponentInstanceUtils.GetInstancePlacementPointElementRefIds(familyInstance);
 
-            for (int i = 0; i < points.Count; i += 1)
-            {
-                //取得的参照点
-                ReferencePoint referencePoint = uiDoc.Document.GetElement(adaptivePoints[i]) as ReferencePoint;
-                //设置参照点坐标
-                referencePoint.Position = points[i];
-            }
+
+
+
+            //Line line = modelLine.GeometryCurve as Line;
+
+
+            Line line = Line.CreateBound(p1, p2);
+
+            FamilyInstance familyInstance = revitDoc.Create.NewFamilyInstance(reference, line, familySymbol);
         }
+
+
+
 
 
         //射线法找元素
